931b149b71917ce3db53f552fe5297f8a8e0c888
dr1gonfir5 <50284158+dr1gonfir5@users.noreply.github.com>
2019-05-12 07:04:52 -0400

    Add files via upload
    Added a while True: loop so the program can be re-played instantly
    Added escape character indents to allow for better readability

--- RPSNew.py
+++ RPSNew.py
@@ -1,33 +1,32 @@
 import random
-rand_num = random.randint(0,2)
-if rand_num == 0:
-    computer = "rock"
-elif rand_num == 1:
-    computer = "paper"
-else:
-    computer = "scissors"
-print("Rock...")
-print("Paper...")
-print("Scissors...")
+while True:
+    rand_num = random.randint(0,2)
+    if rand_num == 0:
+        computer = "rock"
+    elif rand_num == 1:
+        computer = "paper"
+    else:
+        computer = "scissors"
+    print("Rock...\nPaper...\nScissors...\n")
 
-player1 = input("Player, make your move!").lower()
+    player1 = input("Player, make your move!\n").lower()
 
-if player1 == computer:
-    print("It's a draw!")
-elif player1 == "rock":
-    if computer == "scissors":
-        print("Computer picked scissors... You won!")
-    elif computer == "paper":
-        print("Computer picked paper... You lost!")
-elif player1 == "paper":
-    if computer == "rock":
-        print("Computer picked rock... You won!")
-    elif computer == "scissors":
-        print("Computer picked scissors... You lost!")
-elif player1 == "scissors":
-    if computer == "paper":
-        print("Computer picked paper... You win!")
-    elif computer == "rock":
-        print("Computer picked rock... You lost!")
-else:
-    print("Something went wrong. Please type 'rock', 'paper', or 'scissors'.")
+    if player1 == computer:
+        print("\nIt's a draw!")
+    elif player1 == "rock":
+        if computer == "scissors":
+            print("\nComputer picked scissors... You won!")
+        elif computer == "paper":
+            print("\nComputer picked paper... You lost!")
+    elif player1 == "paper":
+        if computer == "rock":
+            print("\nComputer picked rock... You won!")
+        elif computer == "scissors":
+            print("\nComputer picked scissors... You lost!")
+    elif player1 == "scissors":
+        if computer == "paper":
+            print("\nComputer picked paper... You win!")
+        elif computer == "rock":
+            print("\nComputer picked rock... You lost!")
+    else:
+        print("\nSomething went wrong. Please type 'rock', 'paper', or 'scissors'.")
